6d9fde996afa4b8a5469655888a5dc35c7d5696e
karst-larsen <karsten.oneill.larsen@mail.utoronto.ca>
2022-12-04 16:42:30 -0500

    added db.js config file to connect to mongoDB

---
+++ commerce-backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- commerce-backend/server.js
+++ commerce-backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 import cors from "cors";
 
 const app = express();
 dotenv.config();
 app.use(cors());
+connectDB();
 
 const PORT = process.env.PORT || 8080;
 

0dc3f0095df664fcf49a72f0f94edbba1609f142
karst-larsen <karsten.oneill.larsen@mail.utoronto.ca>
2022-12-06 14:30:17 -0500

    added route and controller logic for user sign up, including logic for hashing a password for user security with custom middleware

--- commerce-backend/models/userModel.js
+++ commerce-backend/models/userModel.js
@@ -27,6 +27,20 @@ const userSchema = mongoose.Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  // Checks if password is already modified / hashed, to prevent double hashing
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // Salt hashes the password asynchronously
+  const salt = await bcrypt.genSalt(10);
+
+  // Initially plain text password, but now hashed, with salt as the second argument
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };

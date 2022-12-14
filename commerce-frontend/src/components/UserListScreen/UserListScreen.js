03753136bf7c9d30536da198d0c0b2b2640ffe7f
karst-larsen <karsten.oneill.larsen@mail.utoronto.ca>
2022-12-14 09:07:04 -0500

    added security measure with log out reset as admin, so that the user list clears the user array in state and cannot be seen once logged out

--- commerce-frontend/src/components/UserListScreen/UserListScreen.js
+++ commerce-frontend/src/components/UserListScreen/UserListScreen.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,13 +9,21 @@ import { listUsers } from "../../actions/userActions";
 
 const UserListScreen = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      navigate("/login");
+    }
+  }, [dispatch, navigate, userInfo]);
 
   const deleteHandler = (id) => {
     console.log(id);

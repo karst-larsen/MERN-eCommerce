cac82245aa8bc26c807b7b592b02afc60680f593
karst-larsen <karsten.oneill.larsen@mail.utoronto.ca>
2022-12-14 09:38:54 -0500

    created backend routes for getting a user by id as an admin, and updating a user's information

--- commerce-backend/controllers/userController.js
+++ commerce-backend/controllers/userController.js
@@ -78,7 +78,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 /**
  * Update user profile
  * PUT /api/users/profile
- * Private
+ * Private/Admin
  */
 
 const updateUserProfile = asyncHandler(async (req, res) => {
@@ -134,6 +134,51 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+/** Get user by ID
+ *  GET /api/users/:id
+ *  Private/Admin
+ */
+
+const getUserById = asyncHandler(async (req, res) => {
+  // Find a user by their id, then send everything back except for their password
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+/**
+ * Update user
+ * PUT /api/users/:id
+ * Private/Admin
+ */
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -141,4 +186,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
